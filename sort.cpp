ed934556dc4b33efb1fa86f9c66f05505409b122
Yashwanth R <chawatyash@gmail.com>
2019-05-26 19:17:30 +0530

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -55,6 +55,58 @@ void selectionSort() {
     }
 }
 
+void merge(int arr[], int low, int mid, int high) {
+    int i, j, k;
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[low + i];
+    }
+
+    for (int i = 0; i < n2; i++) {
+        R[i] = arr[mid + 1 + i];
+    }
+
+    i = j = 0;
+    k = low;
+
+    while(i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while(i < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int low, int high) {
+    if (low < high) {
+        int mid = low + (high - low)/2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid+1, high);
+
+        merge(arr, low, mid, high);
+    }
+}
+
 
 int main() {
     insertionSort();

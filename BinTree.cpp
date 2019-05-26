fb796252d0c3f3c3507dcea437151b00a3a5a953
Yashwanth R <chawatyash@gmail.com>
2019-05-27 00:51:41 +0530

    Update BinTree.cpp

--- BinTree.cpp
+++ BinTree.cpp
@@ -1,43 +1,51 @@
 #include <iostream>
 using namespace std;
 
-class binTree {
+class BinTree {
     struct node {
         int val;
-        node * left;
-        node * right;
+        struct node * left;
+        struct node * right;
     };
 
-
     public:
-        struct node * root = new node;
-        binTree(int val) {
+        struct node * root = new struct node;
+        
+        BinTree(int val) {
             this->root->val = val;
             this->root->left = this->root->right = NULL;
         }
 
         void insert(int val) {
-            struct node * n = this->root;
-            while(n != NULL) {
-                if (val < n->left->val) {
-                    n = n->left;
-                } else if (val > n->right->val) {
-                    n = n->right;
+            struct node * tmp = root;
+            while (1) {
+                if (tmp->val > val && tmp->left != NULL) {
+                    tmp = tmp->left;
+                } else if (tmp->val < val && tmp->right != NULL){
+                    tmp = tmp->right;
+                } else {
+                    break;
                 }
             }
 
-            // struct node * N = new node;
-            // N->val = val;
-            // N->left = N->right = NULL;
-            // n = N;
-        }    
+            struct node * N = new struct node;
+            N->val = val;
+            N->left = N->right = NULL;
+
+            if (val < tmp->val) {
+                tmp->left = N;
+            } else if (val > tmp->val) {
+                tmp->right = N;
+            }
+        }
 
 };
 
-int main() {
-    binTree b = binTree(10);
-    b.insert(20);
-    // b.insert(5);
 
-    // cout << b.root->left->val << endl;
+int main() {
+    BinTree b(10);
+    b.insert(5);
+    b.insert(20);
+
+    cout << b.root->right->val << endl;
 }
